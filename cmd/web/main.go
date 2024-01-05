db7eb4acc39ac6acafac723809f849544d78c117
jdonahue135 <jdonahue135@gmail.com>
2024-01-05 13:18:00 -0500

    Add go templates and refactor

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/alexedwards/scs/v2"
-	"github.com/jdonahue135/bookings-app/pkg/config"
-	"github.com/jdonahue135/bookings-app/pkg/handlers"
-	"github.com/jdonahue135/bookings-app/pkg/render"
+	"github.com/jdonahue135/bookings-app/internal/config"
+	"github.com/jdonahue135/bookings-app/internal/handlers"
+	"github.com/jdonahue135/bookings-app/internal/render"
 	"log"
 	"net/http"
 	"time"
